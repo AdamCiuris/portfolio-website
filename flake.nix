b8779e3e4cba12d655618f98d237894b83d1767d
Adam Ciuris <adamciuris@gmail.com>
2024-05-27 08:26:52 -0500

    shell echo stuff

--- flake.nix
+++ flake.nix
@@ -4,22 +4,27 @@
   };
   outputs = { self, nixpkgs, ... }@inputs: # @inputs let's us access the inputs anywhere in the file
     let 
+      pkgList = with pkgs;[
+        python312
+      ] ++
+      (with pkgs.python312Packages; [
+        ipython
+        django
+        pandas
+        numpy
+      ]);
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ 
-          python312
-          ] ++ (with pkgs.python312Packages; [
-            ipython
-            django
-            pandas
-            numpy
-          ]);
-          shellHook = ''
-            echo "my first shell hook"
-          '';
+        buildInputs = pkgList;
+        shellHook = ''
+          echo "You are in a ${system} shell with ${
+            pkgs.lib.concatStrings  # concatenate all strings in the list so they can be coerced to a single string
+              (builtins.map(x: "\n" + x) pkgList) # prepend new line to each package name 
+              }."
+        '';
       };
     };
 }
